a66cba2d78980a2d7e1bbdee854efb3951354d67
Fernando Condino <fer.condino@gmail.com>
2020-07-18 21:35:43 -0300

    adds convenient function: get_single_stratified_split

--- dbpedia_common.py
+++ dbpedia_common.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import seaborn as sns
 import os
+import numpy as np
 from IPython.display import display_html
+from sklearn.model_selection import StratifiedShuffleSplit
 
 sns.set_palette('husl')
 sns.set(style='whitegrid')
@@ -32,6 +34,21 @@ def display_side_by_side(*args):
     for df in args:
         html_str += df.to_html()
     display_html(html_str.replace('table', 'table style="display:inline"'), raw=True)
+
+
+def add_categorical_target(df, column_name):
+    df[column_name] = pd.cut(np.floor(np.log10(df['target'])),
+                             bins=[0., 3.99, 4.99, 5.99, 6.99, np.inf],
+                             labels=[3, 4, 5, 6, 7])
+    return df
+
+
+def get_single_stratified_split(train_df, test_size=0.2, random_state=42):
+    df = train_df.copy()
+    df = add_categorical_target(df, 'target_category')
+    split = StratifiedShuffleSplit(n_splits=1, test_size=test_size, random_state=random_state)
+    train_index, test_index = next(split.split(df, df['target_category']))
+    return train_index, test_index
 
 
 class DataFrameRepository:
